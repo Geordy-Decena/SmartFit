756deb5b69f0e779e38f6cc575c7cf7a3cd69d7a
Geordy Decena <geordydecena@Geordys-MacBook-Pro.local>
2020-06-23 23:31:31 -0400

    deploy

--- client/src/components/SmartExerciseList.js
+++ client/src/components/SmartExerciseList.js
@@ -40,21 +40,21 @@ class SmartExerciseList extends Component {
                     <div style={{ display: 'inline-block', width: '50%', marginRight: '4vh' }} >
                         <h1 style={{ border: '2px solid black', textAlign: 'center', marginRight: '0%', borderRadius: '10px', padding: '.5vw', marginBottom: '2vh', fontSize: '30px', backgroundColor: 'lightgray' }}>Exercise</h1>
                         {this.state.list.map(list =>
-                            list.id === getList() ?
+                            list.id == getList() ?
                                 <h1 style={{ border: '2px solid black', textAlign: 'center', marginRight: '0%', borderRadius: '10px', padding: '.5vw', marginBottom: '2vh', fontSize: '30px' }} key={list.id}>{list.name}</h1> : null
                         )}
                     </div>
                     <div style={{ display: 'inline-block', width: '20%', marginRight: '4vh' }}>
                         <h1 style={{ border: '2px solid black', textAlign: 'center', marginRight: '0%', borderRadius: '10px', padding: '.5vw', marginBottom: '2vh', fontSize: '30px', backgroundColor: 'lightgray' }}>Reps</h1>
                         {this.state.list.map(list =>
-                            list.id === getList() ?
+                            list.id == getList() ?
                                 <h1 style={{ border: '2px solid black', textAlign: 'center', marginRight: '0%', borderRadius: '10px', padding: '.5vw', marginBottom: '2vh', fontSize: '30px' }} key={list.id}>{list.reps}</h1> : null
                         )}
                     </div>
                     <div style={{ display: 'inline-block', width: '20%' }}>
                         <h1 style={{ border: '2px solid black', textAlign: 'center', marginRight: '0%', borderRadius: '10px', padding: '.5vw', marginBottom: '2vh', fontSize: '30px', backgroundColor: 'lightgray' }}>Sets</h1>
                         {this.state.list.map(list =>
-                            list.id === getList() ?
+                            list.id == getList() ?
                                 <h1 style={{ border: '2px solid black', textAlign: 'center', marginRight: '0%', borderRadius: '10px', padding: '.5vw', marginBottom: '2vh', fontSize: '30px' }} key={list.id}>{list.sets}</h1> : null
                         )}
                     </div>
