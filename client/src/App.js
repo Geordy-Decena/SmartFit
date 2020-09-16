a0e0653ebbbe855497972fb61bcbdd762c49bbfc
Geordy Decena <geordydecena@MacBook-Pro-3.local>
2020-09-16 02:55:53 -0400

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -37,61 +37,57 @@ function App() {
 
   return (
     <Fragment>
-      {console.log("WIDTH", windowDimensions.width)}
-      {(windowDimensions.width > 1250) ? (
-        <div className="loadingLargeScreen">
+
+      <Transition
+        timeout={6000}
+        in={true}
+        appear
+      >{(status) => (
+        <div className={`loading loading-${status}`}>
+          <div className={`loadingModal loadingModal-${status}`}></div>
+          <div className="loadingLogoDiv">
+            <div className={`loadingLogo loadingLogo-${status}`}> <img alt="desc" src={Logo} className={`loadingLogoImg loadingLogoImg-${status}`}></img></div>
+          </div>
+          <div className="loadingDotDiv">
+            <div className={`loadingDot1 loadingDot1-${status}`}></div>
+            <div className={`loadingDot2 loadingDot2-${status}`}></div>
+            <div className={`loadingDot3 loadingDot3-${status}`}></div>
+          </div>
+        </div>
+      )}
+      </Transition>
+      {((windowDimensions.width > 1250)) ? (
+        <Transition
+          timeout={8000}
+          in={true}
+          appear
+        >{(status) => (
+          <div className={`loadingLargeScreen loadingLargeScreen-${status}`}>
+            <div className="componentsBigScreen">
+              <Header />
+              <Landing />
+              <Experience />
+              <Projects />
+              <AboutMe />
+            </div>
+          </div>
+        )}
+        </Transition>
+      ) : (
           <Transition
-            timeout={6000}
+            timeout={8000}
             in={true}
             appear
           >{(status) => (
-            <div className={`loading loading-${status}`}>
-              <div className={`loadingModal loadingModal-${status}`}></div>
-              <div className="loadingLogoDiv">
-                <div className={`loadingLogo loadingLogo-${status}`}> <img alt="desc" src={Logo} className={`loadingLogoImg loadingLogoImg-${status}`}></img></div>
-              </div>
-              <div className="loadingDotDiv">
-                <div className={`loadingDot1 loadingDot1-${status}`}></div>
-                <div className={`loadingDot2 loadingDot2-${status}`}></div>
-                <div className={`loadingDot3 loadingDot3-${status}`}></div>
-              </div>
+            <div className={`loadingSmallScreen loadingSmallScreen-${status}`}>
+              <HeaderS />
+              <LandingS />
+              <ExperienceS />
+              <ProjectsS />
+              <AboutMeS />
             </div>
           )}
           </Transition>
-          <div>
-            <Header />
-            <Landing />
-            <Experience />
-            <Projects />
-            <AboutMe />
-          </div>
-        </div>
-      ) : (
-          <div>
-            <Transition
-              timeout={6000}
-              in={true}
-              appear
-            >{(status) => (
-              <div className={`loading loading-${status}`}>
-                <div className={`loadingModal loadingModal-${status}`}></div>
-                <div className="loadingLogoDiv">
-                  <div className={`loadingLogo loadingLogo-${status}`}> <img alt="desc" src={Logo} className={`loadingLogoImg loadingLogoImg-${status}`}></img></div>
-                </div>
-                <div className="loadingDotDiv">
-                  <div className={`loadingDot1 loadingDot1-${status}`}></div>
-                  <div className={`loadingDot2 loadingDot2-${status}`}></div>
-                  <div className={`loadingDot3 loadingDot3-${status}`}></div>
-                </div>
-              </div>
-            )}
-            </Transition>
-            <HeaderS />
-            <LandingS />
-            <ExperienceS />
-            <ProjectsS />
-            <AboutMeS />
-          </div>
         )}
 
     </Fragment>
